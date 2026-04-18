c9cb201f350aa5bc95bef70380492018796bdbad
Yash Raj <yttfc29062024@email.com>
2026-04-18 16:20:22 +0530

    fetch top 5 HN stories dynamically

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import requests
-a="https://hacker-news.firebaseio.com/v0/item/47804965.json?print=pretty"
+a="https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
 response=requests.get(a)
 data=response.json()
-print(data["title"])
+for i in range(5):
+    story_id=data[i]
+    f=f"https://hacker-news.firebaseio.com/v0/item/{story_id}.json"
+    r=requests.get(f)
+    story=r.json()
+    print(story["title"])
+
